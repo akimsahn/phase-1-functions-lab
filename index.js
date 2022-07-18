647ef8cdc05d24e811452fa390f4a16132c56f2e
Allison Kim <akeem159@yahoo.com>
2022-07-18 17:36:18 -0400

    commit tree aaef0c2311cada0707efd4f421bf9503bbabe207

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    return Math.abs(42 - value);
+}
+
+function distanceFromHqInFeet(value) {
+    return Math.abs(distanceFromHqInBlocks(value) * 264);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((end - start) * 264);
+}
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start, end);
+    if (distance < 400) {
+        return 0
+    } else if (distance > 2500) {
+        return 'cannot travel that far'
+    } else if (distance > 2000) {
+        return 25
+    } else {
+        return (distance - 400) * 0.02
+    }
+}
